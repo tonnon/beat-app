91f1b5873928c7318a1a561f9d91824323246226
Lucas Rodrigues <ha-RodriLuc01@electrolux.com>
2025-10-30 17:53:17 -0300

    storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,38 @@
+import type { Preview } from '@storybook/react-vite'
+import '@radix-ui/themes/styles.css'
+import '../src/index.scss'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#f5f8fb',
+        },
+        {
+          name: 'white',
+          value: '#ffffff',
+        },
+        {
+          name: 'dark',
+          value: '#1f2937',
+        },
+      ],
+    },
+
+    a11y: {
+      test: 'todo'
+    }
+  },
+};
+
+export default preview;
